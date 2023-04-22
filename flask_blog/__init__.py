9f36595617c309955af0eef5fb4fa1f5e2ef1981
GL <newkp9@gmail.com>
2023-04-22 23:29:43 +0300

    main, first blueprint

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_blog import config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -10,4 +11,9 @@ def create_app():
     print(__name__)
     app = Flask(__name__)
     db.init_app(app)
+
+    from flask_blog import main
+    app.register_blueprint(main)
+    app.config.from_object(config)
+
     return app
