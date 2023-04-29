39871f184a18c0e892cb3dd384e2682a4e1d2e01
GL <newkp9@gmail.com>
2023-04-29 14:22:35 +0300

    login form

--- flask_blog/users/forms.py
+++ flask_blog/users/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from flask_blog.models import User
@@ -21,3 +21,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=email.date).first()
         if user:
             raise ValidationError('Это имя занято. Выберите другое.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    remember = BooleanField('Напомнить пароль')
+    submit = SubmitField('Войти')
+
