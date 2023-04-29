cfe15163ecc9c282235ef3d450345296c9694302
GL <newkp9@gmail.com>
2023-04-29 14:41:44 +0300

    update acc form

--- flask_blog/users/forms.py
+++ flask_blog/users/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -12,15 +13,17 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Подтвердить пароль:', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Зарегистрироваться')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.date).first()
+    @staticmethod
+    def validata_username(username):
+        user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Это имя занято. Выберите другое.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(username=email.date).first()
+    @staticmethod
+    def validata_email(email):
+        user = User.query.filter_by(username=email.data).first()
         if user:
-            raise ValidationError('Это имя занято. Выберите другое.')
+            raise ValidationError('Пользователь с таким email-ом уже существует.')
 
 
 class LoginForm(FlaskForm):
@@ -29,3 +32,23 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Напомнить пароль')
     submit = SubmitField('Войти')
 
+
+class UpdataAccountForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=2, max=64)])
+    email = StringField('Email:', validators=[DataRequired(), Email()])
+    picture = FileField('Обновить фото профиля', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Обновить')
+
+    @staticmethod
+    def validata_username(username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Это имя занято. Выберите другое.')
+
+    @staticmethod
+    def validata_email(email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(username=email.data).first()
+            if user:
+                raise ValidationError('Пользователь с таким email-ом уже существует.')
