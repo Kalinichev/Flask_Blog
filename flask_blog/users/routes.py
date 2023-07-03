74c8b61ff6525bdbed82aaca05abd2487953812e
GL <newkp9@gmail.com>
2023-07-03 23:37:24 +0300

    login works

--- flask_blog/users/routes.py
+++ flask_blog/users/routes.py
@@ -1,9 +1,9 @@
 from flask_blog import db, bcrypt
 from flask import Blueprint, redirect, url_for, flash, render_template
-from flask_login import current_user
+from flask_login import current_user, login_user
 
 from flask_blog.models import User
-from flask_blog.users.forms import RegistrationForm
+from flask_blog.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint('users', __name__)
 
@@ -19,5 +19,20 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Ваша учетная запись создана', 'success')
-        return redirect(url_for('main.home'))
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('main.home'))
+        else:
+            flash('Войти не удалось. Пожалуйста, проверьте электронную почту и пароль', 'внимание')
+    return render_template('login.html', title='Авторизация', form=form)
